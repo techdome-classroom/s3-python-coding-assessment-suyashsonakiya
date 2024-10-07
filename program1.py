4a162bcc3ff7732c006adeb92761f3ac64cdf9b9
Suyash Sonakiya <suyashsonakiya@gmail.com>
2024-10-07 15:59:06 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,15 +1,11 @@
 class Solution(object):
-    def isValid(s):
-    stack = []
-    pairs = {')': '(', ']': '[', '}': '{'}
-    
-    for char in s:
-        if char in pairs.values():
-            stack.append(char)
-        elif char in pairs.keys():
-            if stack == [] or pairs[char] != stack.pop():
-                return False
-    return stack == []
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pass
+
 
 
 
